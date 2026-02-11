5bca76396e92968507d05f5980b0b9191943d008
TianzhuMo <nuaamtz@nuaa.edu.cn>
2026-02-12 00:51:09 +0800

    模块2

--- mymodule.py
+++ mymodule.py
@@ -2,3 +2,18 @@
 
 def myprint(par):
     print("Hello World!") 
+
+
+# 在Python中，一个.py文件就称之为一个模块（Module）
+
+
+
+# mycompany                <----1.顶层包名
+#  ├─ web                    <----2. 次层包名
+#  │  ├─ __init__.py      <----3. 每个包都需要有一个__init__.py
+#  │  ├─ utils.py            <----4. 模块1
+#  │  └─ www.py
+#  ├─ __init__.py
+#  ├─ abc.py               <----5. 模块N
+#  └─ utils.py
+
